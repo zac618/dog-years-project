2a40bf47cf8be44d4c930c65c04137eefc6b4f32
Zac E <zacegleston@gmail.com>
2022-04-28 19:20:27 -0500

    Create main.js

---
+++ main.js
@@ -0,0 +1,23 @@
+// Declaring variable for my age
+const myAge = 27;
+
+// Declaring value for early years
+let earlyYears = 2;
+
+// Calculating the total years after the early years
+earlyYears = earlyYears * 10.5;
+
+// Taking the early years used off my age
+let laterYears = myAge - 2;
+
+// Multiplying later years by 4 to get dog years
+laterYears *= 4;
+
+// Calculating my age in dog years
+myAgeInDogYears = earlyYears + laterYears;
+
+// Making name all lower case
+let myName = "Zac"; 
+
+// Logging final statement to console
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`)
